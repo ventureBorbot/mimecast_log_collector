02cd5da7cf724f43d399b68507af98aa955e1e6c
ventureBorbot <46605908+ventureBorbot@users.noreply.github.com>
2020-02-09 09:13:34 -0600

    update code

--- configuration.py
+++ configuration.py
@@ -1,15 +1,15 @@
 source_details = {
     "siem_events" : True,
-    "ttp_events" : True,
-    "audit_events" : True
+    "ttp_events" : False,
+    "audit_events" : False
 }
 
 authenication_details = {
-    'APP_ID': "$",
-    'APP_KEY': "$",
-    'EMAIL_ADDRESS' : '$',
-    'ACCESS_KEY': '$',
-    'SECRET_KEY': '$',
+    'APP_ID': "d4ac9e3d-3460-4a6a-be32-790d996f2123",
+    'APP_KEY': "f6145b08-2916-4914-8789-84f9ecc92a10",
+    'EMAIL_ADDRESS' : 'rborman@usventure.com',
+    'ACCESS_KEY': '6BJz90R6hI_YctdB7lYWni9l2VgzFn_ZG98kLqwPOm0ljW7B0QPurf9XyvXFbdot3snPUwa6LhV_7et7kZDhyQNeNFu0-d6CrcToFhgbXZHbjDG8MnRHndPqFhINBziz7TFa6y5OEH7DRTAddfP9jvn1zRB0e4nBLmh52qXFBq_Ycys1k1FW2y3FCAk8pIPo',
+    'SECRET_KEY': 'EVrEp8g1Zi4jtkHs3IIYnFMd79axtsA+nG2HSa2euhrfcV6yHdEzw+WHuqT51HXFPVwOm+zB03rB1SIn4UZoYA==',
 }
 
 syslog_details = {
--- run.py
+++ run.py
@@ -14,8 +14,11 @@ for k, v in configuration.source_details.items():
 
 print("\nStarting main process shortly in 10 seconds\n")
 time.sleep(10)
+#%%
 
 
-for i in source_list:
-    p = multiprocessing.Process(target=lambda: __import__(i))
-    p.start()
+#%%
+#for i in source_list:
+#
+#    p = multiprocessing.Process(target=lambda:__import__(i))
+#    p.start()
